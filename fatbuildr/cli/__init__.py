a8b6aab99f63ccd69127e308ea9a6a6845b9e393
Rémi Palancher <remi@rackslab.io>
2021-12-29 22:00:08 +0100

    Add fatbuildrctl args plumbing with conf on parent
    The two CLI apps now share a common parent FatbuildrCliApp that hold a
    RuntimeConf. The Fatbuildrctl application has its arguments parsing
    plumbling using argparse subparsers for all commands.

--- fatbuildr/cli/__init__.py
+++ fatbuildr/cli/__init__.py
@@ -17,11 +17,76 @@
 # You should have received a copy of the GNU General Public License
 # along with Fatbuildr.  If not, see <https://www.gnu.org/licenses/>.
 
-class Fatbuildrd(object):
+import argparse
+import sys
+from ..version import __version__
+from ..conf import RuntimeConf
 
-    def run():
+
+class FatbuildrCliApp(object):
+
+    @classmethod
+    def run(cls):
+        """Instanciate and execute the CliApp."""
+        app = cls()
+        app.exec()
+
+    def __init__(self):
+        self.conf = RuntimeConf()
+
+
+class Fatbuildrd(FatbuildrCliApp):
+
+    def __init__(self):
+        super().__init__()
+
+    def exec(self):
         print("running fatbuildrd")
 
-class Fatbuildrctl(object):
-    def run():
-        print("running fatbuildrctl")
+
+class Fatbuildrctl(FatbuildrCliApp):
+
+    def __init__(self):
+        super().__init__()
+
+    def exec(self):
+        parser = argparse.ArgumentParser(description='Do something with fatbuildr.')
+        #parser.add_argument('action', help='Action to perform', choices=['build', 'list', 'watch'])
+        parser.add_argument('-v', '--version', dest='version', action='version', version='%(prog)s ' + __version__)
+        parser.add_argument('--debug', dest='debug', action='store_true', help="Enable debug mode")
+
+        subparsers = parser.add_subparsers(help='Action to perform', required=True)
+
+        # create the parser for the build command
+        parser_build = subparsers.add_parser('build', help='Submit new build job')
+        parser_build.add_argument('-p', '--package', help='Package name', required=True)
+        parser_build.add_argument('-b', '--basedir', help='Artefacts definitions directory', required=True)
+        parser_build.set_defaults(func=self._run_build)
+
+        # create the parser for the list command
+        parser_list = subparsers.add_parser('list', help='List build jobs')
+        parser_list.add_argument('-p', '--pending', help='List pending jobs')
+        parser_list.set_defaults(func=self._run_list)
+
+        # create the parser for the watch command
+        parser_watch = subparsers.add_parser('watch', help='Watch build jobs')
+        parser_watch.add_argument('--job', help='Job to watch')
+        parser_watch.set_defaults(func=self._run_list)
+
+        try:
+            args = parser.parse_args()
+        except TypeError:
+            # type error is raised when no subparser action is given
+            print("Action must be choosen, see --help for details")
+            sys.exit(1)
+
+        args.func(args)
+
+    def _run_build(self, args):
+        print("running build for package: %s" % (args.package))
+
+    def _run_list(self, args):
+        raise NotImplementedError
+
+    def _run_watch(self, args):
+        raise NotImplementedError
