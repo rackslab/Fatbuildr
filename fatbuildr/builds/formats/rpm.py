rackslab/Fatbuildr

commit 27fe42b95d31e394cea4f86cbdb0ddae4b2b9bfe

Attach InstanceKeyring to RunningInstance
The keyring is now loaded once with instances. The KeyringManager class
is removed as it is useless now.

fatbuildr/builds/formats/rpm.py
@@ -105,7 +105,7 @@ class ArtefactBuildRpm(ArtefactBuild):
         # fatbuildr packages in mock environment.
         keyring_path = os.path.join(self.place, 'keyring.asc')
         with open(keyring_path, 'w+') as fh:
-            fh.write(self.keyring.export())
+            fh.write(self.instance.keyring.export())
 
         cmd = [
             'mock',
@@ -134,22 +134,22 @@ class ArtefactBuildRpm(ArtefactBuild):
         self.contruncmd(cmd)
 
         # Load keys in agent prior to signing
-        self.keyring.load_agent()
+        self.instance.keyring.load_agent()
 
         # sign all RPM packages, including SRPM
         rpm_glob = os.path.join(self.place, '*.rpm')
         for rpm_path in glob.glob(rpm_glob):
             logger.debug(
                 "Signing RPM %s with key %s"
-                % (rpm_path, self.keyring.masterkey.fingerprint)
+                % (rpm_path, self.instance.keyring.masterkey.fingerprint)
             )
             cmd = [
                 'rpmsign',
                 '--define',
                 '%__gpg /usr/bin/gpg',
                 '--define',
-                '%_gpg_name ' + self.keyring.masterkey.userid,
+                '%_gpg_name ' + self.instance.eyring.masterkey.userid,
                 '--addsign',
                 rpm_path,
             ]
-            self.runcmd(cmd, env={'GNUPGHOME': self.keyring.homedir})
+            self.runcmd(cmd, env={'GNUPGHOME': self.instance.keyring.homedir})

fatbuildr/builds/__init__.py
@@ -31,7 +31,6 @@ from ..artefact import ArtefactDefs
 from ..cache import CacheArtefact
 from ..containers import ContainerRunner
 from ..images import Image, BuildEnv
-from ..keyring import KeyringManager
 from ..utils import runcmd
 from ..log import logr
 from .form import BuildForm
@@ -146,8 +145,6 @@ class ArtefactBuild(AbstractServerBuild):
         self.container = ContainerRunner(conf.containers)
         self.image = Image(conf, self.instance.id, self.format)
         self.env = BuildEnv(conf, self.image, self.environment)
-        self.keyring = KeyringManager(conf).keyring(self.instance.id)
-        self.keyring.load()
         self.defs = None  # loaded in prepare()
         self.log = None  # handler on logfile, opened in run()
 

fatbuildr/builds/formats/deb.py
@@ -126,7 +126,7 @@ class ArtefactBuildDeb(ArtefactBuild):
         # fatbuildr repositories.
         keyring_path = os.path.join(self.place, 'keyring.asc')
         with open(keyring_path, 'w+') as fh:
-            fh.write(self.keyring.export())
+            fh.write(self.instance.keyring.export())
 
         dsc_path = os.path.join(
             self.place, self.name + '_' + self.fullversion + '.dsc'

fatbuildr/builds/formats/osi.py
@@ -61,7 +61,7 @@ class ArtefactBuildOsi(ArtefactBuild):
         self.contruncmd(cmd)
 
         # Load keyring in agent
-        self.keyring.load_agent()
+        self.instance.keyring.load_agent()
 
         # Sign checksum file. Note mkosi built-in signature feature (--sign) is
         # not used because, for security reasons, keyring is not available in
@@ -76,7 +76,7 @@ class ArtefactBuildOsi(ArtefactBuild):
             '--output',
             sig_path,
             '--default-key',
-            self.keyring.masterkey.userid,
+            self.instance.keyring.masterkey.userid,
             checksum_path,
         ]
-        self.runcmd(cmd, env={'GNUPGHOME': self.keyring.homedir})
+        self.runcmd(cmd, env={'GNUPGHOME': self.instance.keyring.homedir})

fatbuildr/cli/fatbuildrd.py
@@ -24,7 +24,6 @@ from . import FatbuildrCliRun
 from ..version import __version__
 from ..conf import RuntimeConfd
 from ..protocols import ServerFactory
-from ..keyring import KeyringManager
 from ..instances import Instances
 from ..timer import ServerTimer
 from ..services import ServiceManager
@@ -79,7 +78,6 @@ class Fatbuildrd(FatbuildrCliRun):
     def _run(self):
 
         logger.debug("Running fatbuildrd")
-        self.keyring_mgr = KeyringManager(self.conf)
         self.server = None
         self.sm = ServiceManager()
         self.timer = ServerTimer()
@@ -143,7 +141,6 @@ class Fatbuildrd(FatbuildrCliRun):
         self.server = ServerFactory.get()
         self.server.run(
             self.instances,
-            self.keyring_mgr,
             self.timer,
         )
         logger.info("Stopping server thread")

fatbuildr/instances.py
@@ -24,7 +24,7 @@ import yaml
 from .tasks.manager import ServerTasksManager
 from .registry.manager import RegistryManager
 from .archives import ArchivesManager
-
+from .keyring import InstanceKeyring
 from .utils import Singleton
 from .log import logr
 
@@ -131,6 +131,8 @@ class RunningInstance:
         self.tasks_mgr = ServerTasksManager(self.conf, self)
         self.registry_mgr = RegistryManager(self.conf, self)
         self.archives_mgr = ArchivesManager(self.conf, self)
+        self.keyring = InstanceKeyring(self.conf, self.id)
+        self.keyring.load()
 
     @property
     def userid(self):

fatbuildr/keyring.py
@@ -266,11 +266,3 @@ class InstanceKeyring:
         runcmd(
             cmd, env={'GNUPGHOME': self.homedir}, input=self.passphrase.encode()
         )
-
-
-class KeyringManager:
-    def __init__(self, conf):
-        self.conf = conf
-
-    def keyring(self, instance):
-        return InstanceKeyring(self.conf, instance)

fatbuildr/protocols/dbus/server.py
@@ -235,9 +235,8 @@ class FatbuildrInterface(InterfaceTemplate):
 class FatbuildrMultiplexer(object):
     """The implementation of Fatbuildr Manager."""
 
-    def __init__(self, instances, keyring_mgr, timer):
+    def __init__(self, instances, timer):
         self._instances = instances
-        self.keyring_mgr = keyring_mgr
         self.timer = timer
 
     def instances(self):
@@ -417,18 +416,18 @@ class FatbuildrMultiplexer(object):
     def keyring(self, instance: Str):
         """Returns masterkey information."""
         self.timer.reset()
-        keyring = self.keyring_mgr.keyring(instance)
-        keyring.load()
-        return DbusKeyring.load_from_keyring(keyring.masterkey)
+        return DbusKeyring.load_from_keyring(
+            self._instances[instance].keyring.masterkey
+        )
 
     def keyring_export(self, instance: Str):
         """Returns armored public key of instance keyring."""
         self.timer.reset()
-        return self.keyring_mgr.keyring(instance).export()
+        return self._instances[instance].keyring.export()
 
 
 class DbusServer(object):
-    def run(self, instances, keyring_mgr, timer):
+    def run(self, instances, timer):
 
         # Print the generated XML specification.
         logger.debug(
@@ -437,7 +436,7 @@ class DbusServer(object):
         )
 
         # Create the Fatbuildr multiplexer.
-        multiplexer = FatbuildrMultiplexer(instances, keyring_mgr, timer)
+        multiplexer = FatbuildrMultiplexer(instances, timer)
 
         # Publish the register at /org/rackslab/Fatbuildr.
         BUS.publish_object(

commit 8f8c3bc15f5aa6304d75c10097ac0181a0f6cf15

Fix keyring attribute typo
Introduced in 27fe42b

fatbuildr/builds/formats/rpm.py
@@ -148,7 +148,7 @@ class ArtefactBuildRpm(ArtefactBuild):
                 '--define',
                 '%__gpg /usr/bin/gpg',
                 '--define',
-                '%_gpg_name ' + self.instance.eyring.masterkey.userid,
+                '%_gpg_name ' + self.instance.keyring.masterkey.userid,
                 '--addsign',
                 rpm_path,
             ]