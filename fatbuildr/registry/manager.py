rackslab/Fatbuildr

commit b7c9ffe2c89b038fe7f0ed699c492edda2c31f46

Attach RegistryManager to RunningInstance
RegistryManager are now attached to a specific instance. A new dbus
property is introduced to get the list of instances defined in server
settings.

fatbuildr/registry/manager.py
@@ -32,57 +32,47 @@ class RegistryManager:
         'osi': RegistryOsi,
     }
 
-    def __init__(self, conf):
+    def __init__(self, conf, instance):
         self.conf = conf
+        self.instance = instance
 
-    @property
-    def instances(self):
-        return os.listdir(self.conf.dirs.registry)
+    def formats(self):
+        return os.listdir(
+            os.path.join(self.conf.dirs.registry, self.instance.id)
+        )
 
-    def formats(self, instance):
-        return os.listdir(os.path.join(self.conf.dirs.registry, instance))
-
-    def distributions(self, instance, fmt):
-        registry = RegistryManager.factory(fmt, self.conf, instance)
+    def distributions(self, fmt):
+        registry = self.factory(fmt)
         return registry.distributions
 
-    def derivatives(self, instance, fmt, distribution):
-        registry = RegistryManager.factory(fmt, self.conf, instance)
+    def derivatives(self, fmt, distribution):
+        registry = self.factory(fmt)
         return registry.derivatives(distribution)
 
-    def artefacts(self, instance, fmt, distribution, derivative):
-        registry = RegistryManager.factory(fmt, self.conf, instance)
+    def artefacts(self, fmt, distribution, derivative):
+        registry = self.factory(fmt)
         return registry.artefacts(distribution, derivative)
 
-    def artefact_bins(
-        self, instance, fmt, distribution, derivative, src_artefact
-    ):
-        registry = RegistryManager.factory(fmt, self.conf, instance)
+    def artefact_bins(self, fmt, distribution, derivative, src_artefact):
+        registry = self.factory(fmt)
         return registry.artefact_bins(distribution, derivative, src_artefact)
 
-    def artefact_src(
-        self, instance, fmt, distribution, derivative, bin_artefact
-    ):
-        registry = RegistryManager.factory(fmt, self.conf, instance)
+    def artefact_src(self, fmt, distribution, derivative, bin_artefact):
+        registry = self.factory(fmt)
         return registry.artefact_src(distribution, derivative, bin_artefact)
 
-    def changelog(
-        self, instance, fmt, distribution, derivative, architecture, artefact
-    ):
-        registry = RegistryManager.factory(fmt, self.conf, instance)
+    def changelog(self, fmt, distribution, derivative, architecture, artefact):
+        registry = self.factory(fmt)
         return registry.changelog(
             distribution, derivative, architecture, artefact
         )
 
-    def delete_artefact(
-        self, instance, fmt, distribution, derivative, artefact
-    ):
-        registry = RegistryManager.factory(fmt, self.conf, instance)
+    def delete_artefact(self, fmt, distribution, derivative, artefact):
+        registry = self.factory(fmt, self.conf, instance)
         return registry.delete_artefact(distribution, derivative, artefact)
 
-    @staticmethod
-    def factory(fmt, conf, instance):
+    def factory(self, fmt):
         """Instanciate the appropriate Registry for the given format."""
         if not fmt in RegistryManager._formats:
             raise RuntimeError("format %s unsupported by registries" % (fmt))
-        return RegistryManager._formats[fmt](conf, instance)
+        return RegistryManager._formats[fmt](self.conf, self.instance.id)

fatbuildr/tasks/manager.py
@@ -127,7 +127,6 @@ class ServerTasksManager:
     ):
         task_id = str(uuid.uuid4())  # generate task ID
         task = RegistryArtefactDeletionTask(
-            self.conf,
             self.instance,
             task_id,
             format,

conf/web/templates/index.html.j2
@@ -4,7 +4,7 @@
 <h1>Available instances:</h1>
 <ul>
 {% for instance in instances %}
-  <li><a href="{{ instance }}/registry/">{{ instance }}<a></li>
+  <li><a href="{{ instance.id }}/registry/">{{ instance.id }} ({{ instance.name }})<a></li>
 {% endfor %}
 </ul>
 <a href="instances.json">JSON</a>

fatbuildr/builds/__init__.py
@@ -28,7 +28,6 @@ import requests
 from ..tasks import RunnableTask
 from ..cleanup import CleanupRegistry
 from ..artefact import ArtefactDefs
-from ..registry.manager import RegistryManager
 from ..cache import CacheArtefact
 from ..containers import ContainerRunner
 from ..images import Image, BuildEnv
@@ -139,11 +138,7 @@ class ArtefactBuild(AbstractServerBuild):
         self.name = form.artefact
         self.instance = instance
         self.cache = CacheArtefact(conf, self.instance.id, self)
-        self.registry = RegistryManager.factory(
-            self.format,
-            conf,
-            self.instance.id,
-        )
+        self.registry = self.instance.registry_mgr.factory(self.format)
         # Get the recursive list of derivatives extended by the given
         # derivative.
         self.derivatives = self.instance.pipelines.recursive_derivatives(

fatbuildr/cli/fatbuildrd.py
@@ -28,7 +28,6 @@ from ..keyring import KeyringManager
 from ..instances import Instances
 from ..timer import ServerTimer
 from ..services import ServiceManager
-from ..registry.manager import RegistryManager
 from ..log import logr
 
 logger = logr(__name__)
@@ -80,7 +79,6 @@ class Fatbuildrd(FatbuildrCliRun):
     def _run(self):
 
         logger.debug("Running fatbuildrd")
-        self.registry_mgr = RegistryManager(self.conf)
         self.keyring_mgr = KeyringManager(self.conf)
         self.server = None
         self.sm = ServiceManager()
@@ -145,7 +143,6 @@ class Fatbuildrd(FatbuildrCliRun):
         self.server = ServerFactory.get()
         self.server.run(
             self.instances,
-            self.registry_mgr,
             self.keyring_mgr,
             self.timer,
         )

fatbuildr/instances.py
@@ -22,6 +22,7 @@ from pathlib import Path
 import yaml
 
 from .tasks.manager import ServerTasksManager
+from .registry.manager import RegistryManager
 
 from .utils import Singleton
 from .log import logr
@@ -127,6 +128,7 @@ class RunningInstance:
         self.gpg_email = gpg_email
         self.pipelines = pipelines
         self.tasks_mgr = ServerTasksManager(self.conf, self)
+        self.registry_mgr = RegistryManager(self.conf, self)
 
     @property
     def userid(self):

fatbuildr/protocols/dbus/client.py
@@ -37,6 +37,9 @@ class DbusClient(object):
 
     # instances
 
+    def instances(self):
+        return DbusInstance.from_structure_list(self.proxy.Instances)
+
     def instance(self, id):
         return DbusInstance.from_structure(self.proxy.Instance(id))
 
@@ -66,9 +69,6 @@ class DbusClient(object):
         return self.proxy.PipelinesDerivativeFormats(instance, derivative)
 
     # registries
-
-    def registry_instances(self):
-        return self.proxy.RegistryInstances
 
     def formats(self, instance):
         return self.proxy.Formats(instance)

fatbuildr/protocols/dbus/server.py
@@ -45,6 +45,11 @@ logger = logr(__name__)
 class FatbuildrInterface(InterfaceTemplate):
     """The DBus interface of Fatbuildr."""
 
+    @property
+    def Instances(self) -> List[Structure]:
+        """Returns the instances list."""
+        return DbusInstance.to_structure_list(self.implementation.instances())
+
     def Instance(self, instance: Str) -> Structure:
         """Returns the instance user id."""
         return DbusInstance.to_structure(self.implementation.instance(instance))
@@ -95,11 +100,6 @@ class FatbuildrInterface(InterfaceTemplate):
         return self.implementation.pipelines_derivative_formats(
             instance, derivative
         )
-
-    @property
-    def RegistryInstances(self) -> List[Str]:
-        """The list of available instances."""
-        return self.implementation.registry_instances
 
     def Queue(self, instance: Str) -> List[Structure]:
         """The list of builds in queue."""
@@ -227,64 +227,69 @@ class FatbuildrInterface(InterfaceTemplate):
 class FatbuildrMultiplexer(object):
     """The implementation of Fatbuildr Manager."""
 
-    def __init__(self, instances, registry_mgr, keyring_mgr, timer):
-        self.instances = instances
-        self.registry_mgr = registry_mgr
+    def __init__(self, instances, keyring_mgr, timer):
+        self._instances = instances
         self.keyring_mgr = keyring_mgr
         self.timer = timer
 
+    def instances(self):
+        self.timer.reset()
+        return [
+            DbusInstance.load_from_instance(instance)
+            for instance in self._instances
+        ]
+
     def instance(self, instance: Str):
         self.timer.reset()
-        return DbusInstance.load_from_instance(self.instances[instance])
+        return DbusInstance.load_from_instance(self._instances[instance])
 
     def pipelines_formats(self, instance: Str):
         self.timer.reset()
-        return self.instances[instance].pipelines.formats
+        return self._instances[instance].pipelines.formats
 
     def pipelines_format_distributions(self, instance: Str, format: Str):
         self.timer.reset()
-        return self.instances[instance].pipelines.format_dists(format)
+        return self._instances[instance].pipelines.format_dists(format)
 
     def pipelines_distribution_format(self, instance: Str, distribution: Str):
         self.timer.reset()
-        return self.instances[instance].pipelines.dist_format(distribution)
+        return self._instances[instance].pipelines.dist_format(distribution)
 
     def pipelines_distribution_derivatives(
         self, instance: Str, distribution: Str
     ):
         self.timer.reset()
-        return self.instances[instance].pipelines.dist_derivatives(distribution)
+        return self._instances[instance].pipelines.dist_derivatives(
+            distribution
+        )
 
     def pipelines_distribution_environment(
         self, instance: Str, distribution: Str
     ):
         self.timer.reset()
-        return self.instances[instance].pipelines.dist_env(distribution)
+        return self._instances[instance].pipelines.dist_env(distribution)
 
     def pipelines_derivative_formats(self, instance: Str, derivative: Str):
         self.timer.reset()
-        return self.instances[instance].pipelines.derivative_formats(derivative)
-
-    @property
-    def registry_instances(self):
-        self.timer.reset()
-        return self.registry_mgr.instances
+        return self._instances[instance].pipelines.derivative_formats(
+            derivative
+        )
 
     def queue(self, instance):
         """The list of builds in instance queue."""
         self.timer.reset()
         return [
             DbusSubmittedBuild.load_from_build(_build)
-            for _build in self.instances[instance].tasks_mgr.queue.dump()
+            for _build in self._instances[instance].tasks_mgr.queue.dump()
         ]
 
     def running(self, instance):
         """The list of builds in queue."""
         self.timer.reset()
-        if not self.instances[instance].tasks_mgr.running:
+        if not self._instances[instance].tasks_mgr.running:
             raise ErrorNoRunningBuild()
         return DbusRunningBuild.load_from_build(
-            self.instances[instance].tasks_mgr.running
+            self._instances[instance].tasks_mgr.running
         )
 
     def archives(self, instance):
@@ -292,28 +297,30 @@ class FatbuildrMultiplexer(object):
         self.timer.reset()
         return [
             DbusArchivedBuild.load_from_build(_build)
-            for _build in self.instances[instance].tasks_mgr.archives()
+            for _build in self._instances[instance].tasks_mgr.archives()
         ]
 
     def formats(self, instance: Str):
         self.timer.reset()
-        return self.registry_mgr.formats(instance)
+        return self._instances[instance].registry_mgr.formats()
 
     def distributions(self, instance: Str, fmt: Str):
         self.timer.reset()
-        return self.registry_mgr.distributions(instance, fmt)
+        return self._instances[instance].registry_mgr.distributions(fmt)
 
     def derivatives(self, instance: Str, fmt: Str, distribution: Str):
         self.timer.reset()
-        return self.registry_mgr.derivatives(instance, fmt, distribution)
+        return self._instances[instance].registry_mgr.derivatives(
+            fmt, distribution
+        )
 
     def artefacts(
         self, instance: Str, fmt: Str, distribution: Str, derivative: Str
     ):
         """Get all artefacts in this derivative of this distribution registry."""
         self.timer.reset()
-        artefacts = self.registry_mgr.artefacts(
-            instance, fmt, distribution, derivative
+        artefacts = self._instances[instance].registry_mgr.artefacts(
+            fmt, distribution, derivative
         )
         return [
             DbusArtefact.load_from_artefact(artefact) for artefact in artefacts
@@ -328,7 +335,7 @@ class FatbuildrMultiplexer(object):
         artefact: Structure,
     ):
         self.timer.reset()
-        return self.instances[instance].tasks_mgr.submit_artefact_deletion(
+        return self._instances[instance].tasks_mgr.submit_artefact_deletion(
             fmt,
             distribution,
             derivative,
@@ -346,8 +353,8 @@ class FatbuildrMultiplexer(object):
         """Get all binary artefacts generated by the given source artefact in
         this derivative of this distribution registry."""
         self.timer.reset()
-        artefacts = self.registry_mgr.artefact_bins(
-            instance, fmt, distribution, derivative, src_artefact
+        artefacts = self._instances[instance].registry_mgr.artefact_bins(
+            fmt, distribution, derivative, src_artefact
         )
         return [
             DbusArtefact.load_from_artefact(artefact) for artefact in artefacts
@@ -364,8 +371,8 @@ class FatbuildrMultiplexer(object):
         """Get the source artefact that generated by the given binary artefact
         in this distribution registry."""
         self.timer.reset()
-        artefact = self.registry_mgr.artefact_src(
-            instance, fmt, distribution, derivative, bin_artefact
+        artefact = self._instances[instance].registry_mgr.artefact_src(
+            fmt, distribution, derivative, bin_artefact
         )
         return DbusArtefact.load_from_artefact(artefact)
 
@@ -381,8 +388,8 @@ class FatbuildrMultiplexer(object):
         """Get the changelog of the given artefact and architecture in this
         distribution registry."""
         self.timer.reset()
-        changelog = self.registry_mgr.changelog(
-            instance, fmt, distribution, derivative, architecture, artefact
+        changelog = self._instances[instance].registry_mgr.changelog(
+            fmt, distribution, derivative, architecture, artefact
         )
         return [
             DbusChangelogEntry.load_from_entry(entry) for entry in changelog
@@ -391,7 +398,7 @@ class FatbuildrMultiplexer(object):
     def submit(self, instance: Str, input: Str):
         """Submit a new build."""
         self.timer.reset()
-        submission = self.instances[instance].tasks_mgr.submit(input)
+        submission = self._instances[instance].tasks_mgr.submit(input)
         return submission.id
 
     def keyring_export(self, instance: Str):
@@ -401,7 +408,7 @@ class FatbuildrMultiplexer(object):
 
 
 class DbusServer(object):
-    def run(self, instances, registry_mgr, keyring_mgr, timer):
+    def run(self, instances, keyring_mgr, timer):
 
         # Print the generated XML specification.
         logger.debug(
@@ -410,9 +417,7 @@ class DbusServer(object):
         )
 
         # Create the Fatbuildr multiplexer.
-        multiplexer = FatbuildrMultiplexer(
-            instances, registry_mgr, keyring_mgr, timer
-        )
+        multiplexer = FatbuildrMultiplexer(instances, keyring_mgr, timer)
 
         # Publish the register at /org/rackslab/Fatbuildr.
         BUS.publish_object(

commit 43f0591b19f20f1d3a29cf7afca77d337d5ff7e3

Fix regression in artefact deletion core code
Introduced in b7c9ffe2

fatbuildr/registry/manager.py
@@ -68,7 +68,7 @@ class RegistryManager:
         )
 
     def delete_artefact(self, fmt, distribution, derivative, artefact):
-        registry = self.factory(fmt, self.conf, instance)
+        registry = self.factory(fmt)
         return registry.delete_artefact(distribution, derivative, artefact)
 
     def factory(self, fmt):