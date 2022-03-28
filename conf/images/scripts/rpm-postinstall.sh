0e49a894173ac8f955344db3ab9d7fdaa52c7385
Rémi Palancher <remi@rackslab.io>
2022-03-28 21:20:55 +0200

    Add fatbuildrd user in mock group of rpm image
    This grants fatbuildrd user to run mock in the container.

--- conf/images/scripts/rpm-postinstall.sh
+++ conf/images/scripts/rpm-postinstall.sh
@@ -5,3 +5,6 @@
 # to make it present in rpm DB and make subsequent dnf executions able to
 # detect the release version automatically.
 dnf install -y --releasever 35 fedora-release
+
+echo "Adding Fatbuildr user ${FATBUILDR_USER} to mock group"
+usermod --append --groups mock ${FATBUILDR_USER}
