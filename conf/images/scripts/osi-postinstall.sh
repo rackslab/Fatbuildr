457871cf42ba6600c6e777a867b25724cd5c5686
Rémi Palancher <remi@rackslab.io>
2022-04-25 16:27:41 +0200

    Fix osi PostInstallScript path

---
+++ conf/images/scripts/osi-postinstall.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# As a work-around for bug https://github.com/systemd/mkosi/issues/644, the
+# fedora-release package is re-installed specifying the releasever explicitely
+# to make it present in rpm DB and make subsequent dnf executions able to
+# detect the release version automatically.
+dnf install -y --releasever 35 fedora-release
