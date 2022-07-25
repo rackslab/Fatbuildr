fca09a5045d628d644dc2367d545e0f672cf6aa1
Rémi Palancher <remi@rackslab.io>
2022-07-25 13:08:35 +0200

    Remove workaround for bug fixed in mkosi v13
    The mkosi bug #644 causing uninitialized RPM database is fixed in mkosi
    v13, which is also a requirement for Fatbuildr. Then the workaround can
    be removed.

--- conf/images/scripts/osi-postinstall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# As a work-around for bug https://github.com/systemd/mkosi/issues/644, the
-# fedora-release package is re-installed specifying the releasever explicitely
-# to make it present in rpm DB and make subsequent dnf executions able to
-# detect the release version automatically.
-dnf install -y --releasever 35 fedora-release
