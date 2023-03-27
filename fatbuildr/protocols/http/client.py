rackslab/Fatbuildr

commit b2b38cb29105fc74958d2c4243ff7f6c09ff48e8

feat(cli): support token based auth to REST API
This commit adds to fatbuildrctl command the support of JWT token based
authentication on Fatbuildr REST API. When the token is available at the
path defined in user preferences file or in default path
~/.local/share/fatbuildr/token, it is automatically send in HTTP
requests headers for authentication. The HTTP status codes sent by the
server are checked and permission errors are reported to users.

fatbuildr/protocols/http/client.py
@@ -21,7 +21,7 @@ import requests
 
 from . import JsonInstance, JsonRunnableTask
 from ..client import AbstractClient
-from ...errors import FatbuildrServerError
+from ...errors import FatbuildrServerPermissionError
 from ...log import logr
 from ...console.client import console_http_client
 
@@ -45,39 +45,59 @@ def check_http_errors(method):
 
 
 class HttpClient(AbstractClient):
-    def __init__(self, uri, scheme):
+    def __init__(self, uri, scheme, token):
         super().__init__(uri, scheme)
+        self.token = token
+
+    @property
+    def headers(self):
+        if self.token:
+            return {"Authorization": f"Bearer {self.token}"}
+        else:
+            return None
+
+    def _auth_request(self, call, *args, **kwargs):
+        if 'headers' in kwargs:
+            kwargs['headers'].update(self.headers)
+        else:
+            kwargs['headers'] = self.headers
+        response = call(*args, **kwargs)
+        if response.status_code == 403:
+            raise FatbuildrServerPermissionError(
+                f"{response.json()['error']} (403)"
+            )
+        return response
 
     @check_http_errors
     def instance(self):
         url = f"{self.uri}/instance.json"
-        response = requests.get(url)
+        response = self._auth_request(requests.get, url)
         return JsonInstance.load_from_json(response.json())
 
     @check_http_errors
     def pipelines_architectures(self):
         url = f"{self.uri}/pipelines/architectures.json"
-        response = requests.get(url)
+        response = self._auth_request(requests.get, url)
         return response.json()
 
     @check_http_errors
     def pipelines_format_distributions(self, format):
         url = f"{self.uri}/pipelines/formats.json?format={format}"
-        response = requests.get(url)
+        response = self._auth_request(requests.get, url)
         formats = response.json()
         return [item['distribution'] for item in formats[format]]
 
     @check_http_errors
     def pipelines_distribution_format(self, distribution):
         url = f"{self.uri}/pipelines/formats.json?distribution={distribution}"
-        response = requests.get(url)
+        response = self._auth_request(requests.get, url)
         formats = response.json()
         return list(formats.keys())[0]
 
     @check_http_errors
     def pipelines_distribution_environment(self, distribution):
         url = f"{self.uri}/pipelines/formats.json?distribution={distribution}"
-        response = requests.get(url)
+        response = self._auth_request(requests.get, url)
         formats = response.json()
         # Return the environment of the first distribution of the first format,
         # because there is only one format and distribution thanks to the
@@ -87,7 +107,7 @@ class HttpClient(AbstractClient):
     @check_http_errors
     def pipelines_derivative_formats(self, derivative):
         url = f"{self.uri}/pipelines/formats.json?derivative={derivative}"
-        response = requests.get(url)
+        response = self._auth_request(requests.get, url)
         formats = response.json()
         return list(formats.keys())
 
@@ -113,7 +133,8 @@ class HttpClient(AbstractClient):
         if source_tarball:
             files['source'] = open(source_tarball, 'rb')
 
-        response = requests.post(
+        response = self._auth_request(
+            requests.post,
             url,
             data={
                 'format': format,
@@ -140,7 +161,7 @@ class HttpClient(AbstractClient):
     @check_http_errors
     def queue(self):
         url = f"{self.uri}/queue.json"
-        response = requests.get(url)
+        response = self._auth_request(requests.get, url)
         return [
             JsonRunnableTask.load_from_json(task) for task in response.json()
         ]
@@ -148,7 +169,7 @@ class HttpClient(AbstractClient):
     @check_http_errors
     def running(self):
         url = f"{self.uri}/running.json"
-        response = requests.get(url)
+        response = self._auth_request(requests.get, url)
         json_task = response.json()
         if json_task is None:
             return None
@@ -157,7 +178,7 @@ class HttpClient(AbstractClient):
     @check_http_errors
     def get(self, task_id):
         url = f"{self.uri}/tasks/{task_id}.json"
-        response = requests.get(url)
+        response = self._auth_request(requests.get, url)
         json_task = response.json()
         if json_task is None:
             return None
@@ -173,5 +194,5 @@ class HttpClient(AbstractClient):
     def watch(self, task):
         """Generate task log lines with a streaming request."""
         url = f"{self.uri}/watch/{task.id}.journal"
-        response = requests.get(url, stream=True)
+        response = self._auth_request(requests.get, url, stream=True)
         return console_http_client(response)

fatbuildr/cli/fatbuildrctl.py
@@ -421,7 +421,7 @@ class Fatbuildrctl(FatbuildrCliRun):
         connection and returns it."""
         if self._connection:
             return self._connection
-        self._connection = ClientFactory.get(self.uri)
+        self._connection = ClientFactory.get(self.uri, self.prefs.token)
         return self._connection
 
     def load(self, args):

fatbuildr/prefs.py
@@ -34,6 +34,14 @@ def default_user_pref():
     )
 
 
+def default_user_token():
+    """Returns the default path to the user token file, through XDG_DATA_HOME
+    environment variable if it is set."""
+    return Path(os.getenv('XDG_DATA_HOME', '~/.local/share')).joinpath(
+        'fatbuildr/token'
+    )
+
+
 class UserPreferences:
     DEFAULT = default_user_pref()
 
@@ -65,6 +73,18 @@ class UserPreferences:
             basedir = Path(basedir).expanduser()
         self.basedir = basedir
         self.message = config.get('prefs', 'message', fallback=None)
+        token_path = Path(
+            config.get(
+                'prefs',
+                'token',
+                fallback=default_user_token(),
+            )
+        ).expanduser()
+        if token_path.exists():
+            with open(token_path) as fh:
+                self.token = fh.read().strip()
+        else:
+            self.token = None
 
     def dump(self):
         if not logger.has_debug():
@@ -77,3 +97,4 @@ class UserPreferences:
         logger.debug("   uri: %s", self.uri)
         logger.debug("   basedir: %s", self.basedir)
         logger.debug("   message: %s", self.message)
+        logger.debug("   token: %s", self.token)

fatbuildr/protocols/__init__.py
@@ -29,7 +29,7 @@ from ..errors import FatbuildrRuntimeError
 
 class ClientFactory(object):
     @staticmethod
-    def get(address):
+    def get(address, token=None):
         uri = urlparse(address)
         instance = uri.path.strip('/')
         if uri.scheme == 'dbus':
@@ -39,7 +39,7 @@ class ClientFactory(object):
                 )
             return DBusClient(address, uri.scheme, instance)
         elif uri.scheme in ['http', 'https']:
-            return HttpClient(address, uri.scheme)
+            return HttpClient(address, uri.scheme, token)
         else:
             raise FatbuildrRuntimeError(f"unsupported URI {uri}")
 

commit 9df1d226f4f9ed7346c0d9218820e373b4e52e1f

fix: add missing imported error
Fixes a bug introduced in commit b2b38cb that removed a required
imported error.

fatbuildr/protocols/http/client.py
@@ -21,7 +21,7 @@ import requests
 
 from . import JsonInstance, JsonRunnableTask
 from ..client import AbstractClient
-from ...errors import FatbuildrServerPermissionError
+from ...errors import FatbuildrServerError, FatbuildrServerPermissionError
 from ...log import logr
 from ...console.client import console_http_client
 