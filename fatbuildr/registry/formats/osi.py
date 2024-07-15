rackslab/Fatbuildr

commit 7e5a70f0f9fa3dcb571812720940744bae50e5af

fix: support mkosi v22 for osi artifacts
Latest versions of mkosi bring many improvements, as mkosi does not need
root privileges anymore and it does need loopback device neither. Some
command arguments have changed, the CHECKSUM file name has changed and
it does not save resulting image in a subdirectory anymore. These
changes require some adjustments in osi build and registry classes.

fatbuildr/registry/formats/osi.py
@@ -29,7 +29,7 @@ logger = logr(__name__)
 class RegistryOsi(Registry):
     """Registry for Osi format (aka. OS images)."""
 
-    CHECKSUMS_FILE = 'SHA256SUMS'
+    CHECKSUMS_FILE_EXT = 'SHA256SUMS'
 
     def __init__(self, conf, instance):
         super().__init__(conf, instance, 'osi')
@@ -73,8 +73,12 @@ class RegistryOsi(Registry):
             return results
 
         # Copy or merge checksum file generated by mkosi
-        new_checksum_path = build.subdir.joinpath(self.CHECKSUMS_FILE)
-        derivative_checksum_path = derivative_path.joinpath(self.CHECKSUMS_FILE)
+        new_checksum_path = build.place.joinpath(
+            f"{build.artifact}_{build.version.main}.{self.CHECKSUMS_FILE_EXT}"
+        )
+        derivative_checksum_path = derivative_path.joinpath(
+            self.CHECKSUMS_FILE_EXT
+        )
         if not derivative_checksum_path.exists():
             logger.debug(
                 "Deploying new checksum file %s in registry", new_checksum_path
@@ -139,7 +143,7 @@ class RegistryOsi(Registry):
         # and .cpio, possibly suffixed by compression algorithm (eg. .raw.xz).
         for extension in ['qcow2', 'raw', 'tar', 'cpio']:
             built_files.extend(
-                [_path for _path in build.subdir.glob(f"*.{extension}*")]
+                [_path for _path in build.place.glob(f"*.{extension}*")]
             )
         logger.debug(
             "Found files: %s",

fatbuildr/builds/formats/osi.py
@@ -64,14 +64,6 @@ class ArtifactBuildOsi(ArtifactBuild):
             interactive,
         )
 
-    @property
-    def subdir(self):
-        """mkosi >= 14 systematically creates a subdirectory named after the
-        distribution and the release separated by tilde (~) in the output
-        director. This property returns the path to this subdirectory using a
-        simple glob pattern."""
-        return list(self.place.glob('*~*'))[0]
-
     def build(self):
         """Build the OS image using mkosi"""
 
@@ -83,20 +75,15 @@ class ArtifactBuildOsi(ArtifactBuild):
                 f"Unable to find OS image definition file at {def_path}"
             )
 
-        # mkosi executes systemd-nspawn container to launch commands in the
-        # built image, eg. to remove documentation. By default, systemd-nspawn
-        # connects to DBus system session to register the container on
-        # systemd-machined service and obtain a specific unit dynamically.
-        # Unfortunately, this cannot work inside another container because DBus
-        # system session is not available. This makes systemd-nspawn and mkosi
-        # fail eventually. To avoid this error, mkosi is launched with
-        # --nspawn-keep-unit argument to make it execute underlying
-        # systemd-nspawn with option to avoid registration and communication on
-        # DBus system session.
         cmd = [
             self.image.builder,
-            '--nspawn-keep-unit',
-            '--default',
+            '--directory',
+            self.place.joinpath(self.format),
+            '--package-cache-dir',
+            self.cache.dir,
+            '--workspace-dir',
+            str(self.place),
+            '--include',
             str(def_path),
             '--output-dir',
             str(self.place),
@@ -105,12 +92,10 @@ class ArtifactBuildOsi(ArtifactBuild):
             '--image-version',
             self.version.main,
             '--checksum',
+            'build',
         ]
-        # mkosi requires being run as root (with or without sudo). The SUDO_UID
-        # environment is defined to fake sudo and eventually make mkosi chown
-        # the content of output directory to the user running fatbuildrd.
         if self.image.format_conf.containerized:
-            self.cruncmd(cmd, envs=[f"SUDO_UID={os.getuid()}"], asroot=True)
+            self.cruncmd(cmd)
         else:
             self.runcmd(
                 cmd,
@@ -119,7 +104,3 @@ class ArtifactBuildOsi(ArtifactBuild):
                     'PATH': f"{os.getenv('PATH')}:/bin:/sbin",
                 },
             )
-
-        logger.debug(
-            "Output subdirectory generated by mkosi is %s", self.subdir
-        )

commit dfc03154a3a1a0994df5a8ea93551b00dcecf0a2

fix: regression with RegistryOsi class attribute
Introduced in 7e5a70f.

fatbuildr/registry/formats/osi.py
@@ -166,7 +166,7 @@ class RegistryOsi(Registry):
     def _artifacts_filter(self, distribution, derivative, name_filter=None):
         artifacts = []
         for _path in self.derivative_path(distribution, derivative).iterdir():
-            if _path.name == RegistryOsi.CHECKSUMS_FILE:
+            if _path.suffix == '.' + RegistryOsi.CHECKSUMS_FILE_EXT:
                 continue
             if _path.suffix == '.manifest':
                 continue